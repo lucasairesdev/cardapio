6595eb798f2cfa12509017266afea4e81c3c3f41
Lucas Aires <116677841+lucasairesdev@users.noreply.github.com>
2023-10-05 20:16:10 -0300

    Finalizando o backEnd

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -3,6 +3,7 @@ package com.example.cardapio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ public class FoodController {
 	@Autowired(required = true)
 	private FoodRepository repository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping
 	public void saveFood(@RequestBody FoodRequestDTO data) {
 		Food foodData = new Food(data);
