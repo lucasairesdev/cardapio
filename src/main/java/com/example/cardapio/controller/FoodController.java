7e785d1e111d262fbf050a09de76e053b1b83b9b
Lucas Aires <116677841+lucasairesdev@users.noreply.github.com>
2023-10-05 16:27:36 -0300

    Criando classes/interfaces e corrigindo erro de packages

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,15 +1,27 @@
 package com.example.cardapio.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.model.Food;
+import com.example.cardapio.repository.FoodRepository;
+
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 	
+	@Autowired(required = true)
+	private FoodRepository repository;
+	
+	
 	@GetMapping
-	public void getAll() {
+	public List<Food> getAll() {
 		
+		List<Food> foodList = repository.findAll();
+		return foodList;
 	}
 }
